81c50be9d6b4757346b384e245424355f256deb1
artmay0330 <elmustafa.mayeldeen@gmail.com>
2026-07-25 22:53:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import requests
 
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+CHAT_ID = "YOUR_CHANNEL_ID"
+
+message = "TEST SIGNAL"
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+requests.post(url, data={
+    "chat_id": CHAT_ID,
+    "text": message
+})
